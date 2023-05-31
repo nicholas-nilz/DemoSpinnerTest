bec9e480c06e5441b54b3b91a99c3daabd950ced
nicholas <22014726@myrp.edu.sg>
2023-05-31 12:08:51 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22014726/demospinnertest/MainActivity.java
@@ -0,0 +1,42 @@
+package sg.edu.rp.c346.id22014726.demospinnertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spin;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spin = findViewById(R.id.spinner);
+        tv = findViewById(R.id.textView);
+
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                    tv.setText("Spinner Item, Yes Selected");
+                    break;
+
+                    case 1:
+                    tv.setText("Spinner Item, No Selected");
+                    break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){ }
+            });
+    }
+}
